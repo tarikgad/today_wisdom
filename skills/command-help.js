685615d88edecad825624802f954790289430c8e
Tarik Gad <36502125+tarikgad@users.noreply.github.com>
2026-04-02 23:15:35 +0200

    Update command-help.js

--- skills/command-help.js
+++ skills/command-help.js
@@ -3,27 +3,14 @@
 //
 module.exports = function (controller) {
 
-    controller.hears(["help", "who", "what"], 'message,direct_message,direct_mention', async (bot, message) => {
+    controller.hears(['help', 'who'], 'message,direct_message', async (bot, message) => {
         
-        let text = "Here are my skills:";
-        
-        // Using template literals for cleaner formatting in Webex
-        text += `\n- ${bot.enrichCommand(message, "about")}: shows metadata about myself`;
-        text += `\n- ${bot.enrichCommand(message, "help")}: spreads the word about my skills`;
-        text += `\n- ${bot.enrichCommand(message, "wisdom")}: show Today Wisdom`;
-        
-        await bot.reply(message, text);
-    });
-};//
-// Command: help
-//
-module.exports = function (controller) {
+        let text = "Here are the commands I currently understand:";
+        text += "\n- **about**: shows metadata about myself";
+        text += "\n- **ping**: check if I am awake";
+        text += "\n- **wisdom**: receive a random proverb";
+        text += "\n- **help**: displays this message";
 
-    controller.hears(["help", "who", "what"], 'direct_message,direct_mention', function (bot, message) {
-        var text = "Here are my skills:";
-        text += "\n- " + bot.enrichCommand(message, "about") + ": shows metadata about myself";
-        text += "\n- " + bot.enrichCommand(message, "help") + ": spreads the word about my skills";
-		text += "\n- " + bot.enrichCommand(message, "wisdom") + ": show Today Wisdom";
-        bot.reply(message, text);
+        await bot.reply(message, text);
     });
 }
